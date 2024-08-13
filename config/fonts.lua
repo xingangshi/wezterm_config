7a9bd68aa37f8cb34510d9c58fa0040ec534c104
Panshi <shix_112@126.com>
2024-08-14 00:04:12 +0800

    update font

--- config/fonts.lua
+++ config/fonts.lua
@@ -1,8 +1,8 @@
 local wezterm = require('wezterm')
 local platform = require('utils.platform')
 
-local font = 'Sarasa Mono SC' --'JetBrainsMono Nerd Font'
-local font_size = platform().is_mac and 13 or 9
+local font = 'JetBrainsMonoNl Nerd Font Propo' --'Sarasa Mono SC' --'JetBrainsMono Nerd Font'
+local font_size = platform().is_mac and 13 or 12
 
 return {
    font = wezterm.font(font),
